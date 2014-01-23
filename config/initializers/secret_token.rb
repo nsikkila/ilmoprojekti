3680c1f3cfb34f34b1fc2f15f751c16679ccc458
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-23 19:32:26 +0200

    rails-projekti luotu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ilmoprojekti::Application.config.secret_key_base = 'f6a0ff28a8b4eb0e8e9e9180b06d4194c3f16ee2f68f37ae21c011b369582e594bc845cb7c046b23dc2569994fd99c4ee4ef924a2549964cd1a3e087cadfaa68'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ilmoprojekti::Application.config.session_store :cookie_store, key: '_ilmoprojekti_session'
