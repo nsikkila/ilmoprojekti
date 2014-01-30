4403eb258900e36720c111883df422ebde73235a
Sallinen Anniina C <annisall@da3-cs-dk108-04.helsinki.fi>
2014-01-30 13:38:47 +0200

    update user authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,15 +61,20 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    if not current_user.id == params[:id].to_i
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'edit' }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to :root
     end
+
   end
 
   # DELETE /users/1
