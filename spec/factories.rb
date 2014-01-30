4080124d8525803b5f8b5b576cd979f0c7721433
Mika Aho <mjaho@cs.helsinki.fi>
2014-01-30 15:55:38 +0200

    Testirunkoa

--- spec/factories.rb
+++ spec/factories.rb
@@ -13,3 +13,39 @@ FactoryGirl.define do
 		password_confirmation "Test123"
 	end
 end
+
+FactoryGirl.define do
+  factory :signup do
+    student_id 1
+    project_id 1
+    priority 1
+    status true
+  end
+end
+
+FactoryGirl.define do
+  factory :projectbundle do
+  name "Testibundle"
+  description "This is a test bundle created by factorygirl"
+  active true
+  end
+end
+
+FactoryGirl.define do
+  factory :student do
+    firstname "Testi"
+    lastname "Testinen"
+    studentnumber "013460745"
+  end
+end
+
+FactoryGirl.define do
+  factory :project do
+    name "Testproject"
+    description "This is a test project created by factorygirl"
+    maxstudents 15
+    user_id 1
+    bundle_id 1
+  end
+end
+
