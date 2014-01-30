c80e084d67d2e8e9e89e48ae47f57ad625a6622f
Sikkilä Niko P <nsikkila@da3-cs-dk110-08.helsinki.fi>
2014-01-30 11:55:32 +0200

    first working spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+#describe User do
+ # pending "add some examples to (or delete) #{__FILE__}"
+#end
+
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "constructor correctly sets username" do
+		user = User.new username:"Maija"
+
+		user.username.should == "Maija"
+	end
 end
