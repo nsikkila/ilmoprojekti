71b48eeb430dac55d17f6e59f67b2bc39ddbdbb4
Sikkilä Niko P <nsikkila@da3-cs-dk108-05.helsinki.fi>
2014-01-30 12:49:34 +0200

    tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,34 @@
 require 'spec_helper'
 
-#describe User do
+#dfsajsescribe User do
  # pending "add some examples to (or delete) #{__FILE__}"
 #end
 
 describe User do
-	it "constructor correctly sets username" do
+	it "has the username set correctly" do
 		user = User.new username:"Maija"
 
 		user.username.should == "Maija"
+	end
+
+	it "is saved with proper fields" do
+		user = User.create username:"Maija", password:"Testi123", password_confirmation:"Testi123"
+
+		#User is valid
+		expect(user.valid?).to be (true)
+		#User is added to db
+		expect(User.count).to eq (1)
+	end
+
+	describe "without a proper username" do
+		it "is not saved" do
+			#Username is too short
+			user = User.create username:"ab", password:"Testi123", password_confirmation:"Testi123"
+
+			#User must not be valid
+			expect(user.valid?).to be(false)
+			#User must not be added to db
+			expect(User.count).to eq(0)
+		end
 	end
 end
