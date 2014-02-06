90803e6b9bd10fc13129b87730374887a5100d5b
Mika Aho <mjaho@cs.helsinki.fi>
2014-02-06 11:17:04 +0200

    Mockup näkyville

--- app/controllers/casas_controller.rb
+++ app/controllers/casas_controller.rb
@@ -10,4 +10,20 @@ class CasasController < ApplicationController
     @signup5 = Signup.new
     @signup6 = Signup.new
   end
+
+  def create
+    @student = Student.create(firstname: params[:sfirstname], lastname: params[:slastname], studentnumber: params[:studentnumber])
+    Signup.create(student_id: @student.id, project_id: params[:p1][:project_id])
+    Signup.create(student_id: @student.id, project_id: params[:p2][:project_id])
+    Signup.create(student_id: @student.id, project_id: params[:p3][:project_id])
+    Signup.create(student_id: @student.id, project_id: params[:p4][:project_id])
+    Signup.create(student_id: @student.id, project_id: params[:p5][:project_id])
+    Signup.create(student_id: @student.id, project_id: params[:p6][:project_id])
+    @signups = @student.signups
+    render action:'show'
+  end
+
+  def show
+    @signups = Signup.all
+  end
 end
