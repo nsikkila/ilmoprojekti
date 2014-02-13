cc6aed84bad7dda7b430cab112716794f9e25770
Mika Aho <mjaho@cs.helsinki.fi>
2014-02-13 13:03:01 +0200

    Bugfix

--- app/controllers/casas_controller.rb
+++ app/controllers/casas_controller.rb
@@ -2,25 +2,19 @@ class CasasController < ApplicationController
 
   def index
     @Projectbundle = Projectbundle.first
+    #@projects = @Projectbundle.projects
     @projects = Project.all
-    @signup = Signup.new
-    @signup2 = Signup.new
-    @signup3 = Signup.new
-    @signup4 = Signup.new
-    @signup5 = Signup.new
-    @signup6 = Signup.new
   end
 
   def create
 
-
     @student = Student.create(firstname: params[:sfirstname], lastname: params[:slastname], studentnumber: params[:studentnumber], email: params[:email])
-    Signup.create(student_id: @student.id, priority: 1, status: "pending", project_id: params[:p1][:project_id])
-    Signup.create(student_id: @student.id, priority: 2, status: "pending", project_id: params[:p2][:project_id])
-    Signup.create(student_id: @student.id, priority: 3, status: "pending", project_id: params[:p3][:project_id])
-    Signup.create(student_id: @student.id, priority: 4, status: "pending", project_id: params[:p4][:project_id])
-    Signup.create(student_id: @student.id, priority: 5, status: "pending", project_id: params[:p5][:project_id])
-    Signup.create(student_id: @student.id, priority: 6, status: "pending", project_id: params[:p6][:project_id])
+    Signup.create(student_id: @student.id, priority: 1, status: false, project_id: params[:p1][:project_id])
+    Signup.create(student_id: @student.id, priority: 2, status: false, project_id: params[:p2][:project_id])
+    Signup.create(student_id: @student.id, priority: 3, status: false, project_id: params[:p3][:project_id])
+    Signup.create(student_id: @student.id, priority: 4, status: false, project_id: params[:p4][:project_id])
+    Signup.create(student_id: @student.id, priority: 5, status: false, project_id: params[:p5][:project_id])
+    Signup.create(student_id: @student.id, priority: 6, status: false, project_id: params[:p6][:project_id])
     @signups = @student.signups
     render action:'show'
   end
--- spec/controllers/casas_controller_spec.rb
+++ spec/controllers/casas_controller_spec.rb
@@ -8,11 +8,11 @@ describe CasasController do
 
 
   it "#create takes signups to database" do
-    post :create, { sfirstname:"Matti", slastname:"Mainio", studentnumber:'1234567', email:'testi@maili.fi', p1:{ projectid:'1' }, p2:{ projectid:'2' }, p3:{ projectid:'3'}, p4:{ projectid:'4'}, p5:{ projectid:'5' }, p6:{ projectid:'6' } }
-    #six signups
-    expect(Signup.count).to eq(6)
+    post :create, { sfirstname:"Matti", slastname:"Mainio", studentnumber:'1234567', email:'testi@maili.fi', p1:{ project_id:'1' }, p2:{ project_id:'2' }, p3:{ project_id:'3'}, p4:{ project_id:'4'}, p5:{ project_id:'5' }, p6:{ project_id:'6' } }
     #one student
     expect(Student.count).to eq(1)
+    #six signups
+    expect(Signup.count).to eq(6)
     expect(Student.first.signups.first.student_id).to eq(1)
   end
 
