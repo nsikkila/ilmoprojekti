c476582e4b5492e948fc93c372a66edb5a90ee1b
Mika Aho <mjaho@cs.helsinki.fi>
2014-02-13 18:36:17 +0200

    Plaah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,19 @@ Ilmoprojekti::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'ilmoprojekti.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "ilmoprojekti.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "ilmoprojekti",
+      password: "Ohtu2014"
+  }
 end
