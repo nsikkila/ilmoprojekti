502f5ccce2e9426b1787b17f4083ce362fbd374a
Sallinen Anniina C <annisall@da3-cs-dk110-08.helsinki.fi>
2014-02-13 16:41:58 +0200

    added sending email

--- app/mailers/enrollment_mail.rb
+++ app/mailers/enrollment_mail.rb
@@ -1,3 +1,10 @@
 class EnrollmentMail < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "donotreply@ilmoprojekti.com"
+
+  def confirmation_email(user, hash)
+  	@student=user
+  	@digest=hash
+  	@url="http://ilmoprojekti.herokuapp.com/enrollments/"+@student.id.to_s+"/"+@digest
+  	mail(to: @user.email, subject:'Vahvistus ilmottautumisesta')
+  end
 end
