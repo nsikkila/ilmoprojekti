7dee4d81ed0c2ea851a29f68a3cf5b35c50e7bb5
Sallinen Anniina C <annisall@da3-cs-dk110-08.helsinki.fi>
2014-02-13 17:26:12 +0200

    removed route to create

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Ilmoprojekti::Application.routes.draw do
   get 'enrollments/edit/:student_id/:hash', to: 'enrollments#edit'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
-  post '/', to:'enrollments#create'
+
  
 
   root 'enrollments#index'
