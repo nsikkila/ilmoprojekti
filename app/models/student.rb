9c1b2459cfc4fbf8315ac53f49449d7b9f2641b7
Mika Aho <mjaho@cs.helsinki.fi>
2014-02-13 16:35:24 +0200

    Formiin kamaa ja testejä

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,7 @@ class Student < ActiveRecord::Base
 
   validates :firstname, length: {minimum: 2}
   validates :lastname, length: {minimum: 2}
-  validates :studentnumber, length: {minimum: 7}
+  validates_numericality_of :studentnumber, length: {minimum: 7}
   validates :email, presence: {on: :create}, format: {with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i, message: "has to be a valid email address"}
 
 end
---
+++ spec/features/signup_page_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+include TestHelper
+
+describe "Signup page" do
+
+    it "listing cannot be accessed if user is not logged in as at least a teacher" do
+      visit signups_path
+      expect(page).to have_content("Etunimi:")
+    end
+
+    it "can be accessed if the user is logged in as a teacher" do
+      user = FactoryGirl.create :teacher
+      signin(username:user.username, password:user.password)
+
+      visit signups_path
+      expect(page).to have_content("Listing signups")
+    end
+
+  #Turha testi?
+  it "does not allow editing if user is not logged in" do
+    FactoryGirl.create(:signup)
+    visit edit_signup_path(1)
+    expect(page).should_not have_content("Editing signup")
+  end
+
+end
