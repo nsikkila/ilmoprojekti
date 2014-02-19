4bb546a65ace8daa6f41528e275f243909de0afb
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-19 13:09:54 +0200

    enrollment menee kantaan

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -13,6 +13,11 @@ class EnrollmentsController < ApplicationController
 
   def create
 
+    @enrollment = Enrollment.new(enrollment_params)
+
+    @enrollment.save
+
+=begin
     Signup.create(student_id: @student.id, priority: 1, status: false, project_id: params[:p1][:project_id])
     Signup.create(student_id: @student.id, priority: 2, status: false, project_id: params[:p2][:project_id])
     Signup.create(student_id: @student.id, priority: 3, status: false, project_id: params[:p3][:project_id])
@@ -21,7 +26,9 @@ class EnrollmentsController < ApplicationController
     Signup.create(student_id: @student.id, priority: 6, status: false, project_id: params[:p6][:project_id])
     @digest=create_hash(@student)
     EnrollmentMail.confirmation_email(@student, @digest).deliver
+=end
     render action:'show'
+
   end
 
   # GET enrollments/edit/student_id/hash
@@ -51,6 +58,10 @@ class EnrollmentsController < ApplicationController
   end
 
 private
+
+  def enrollment_params
+    params.require(:enrollment).permit(:firstname, :lastname, :studentnumber, :email)
+  end
 
   def create_hash(student)
      Digest::SHA1.hexdigest (student.id.to_s + student.studentnumber.to_s)
