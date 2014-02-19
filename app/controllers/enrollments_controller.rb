ac7faa74b9973cb0e8a6ca9febcfccc1e6fb363c
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-19 12:48:45 +0200

    formin eka versio

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -7,19 +7,18 @@ class EnrollmentsController < ApplicationController
   def new
     @projectbundle = Projectbundle.first
     @projects = @projectbundle.projects
+    @enrollment = Enrollment.new
     #@projects = Project.all
   end
 
   def create
 
-    @student = Student.create(firstname: params[:sfirstname], lastname: params[:slastname], studentnumber: params[:studentnumber], email: params[:email])
     Signup.create(student_id: @student.id, priority: 1, status: false, project_id: params[:p1][:project_id])
     Signup.create(student_id: @student.id, priority: 2, status: false, project_id: params[:p2][:project_id])
     Signup.create(student_id: @student.id, priority: 3, status: false, project_id: params[:p3][:project_id])
     Signup.create(student_id: @student.id, priority: 4, status: false, project_id: params[:p4][:project_id])
     Signup.create(student_id: @student.id, priority: 5, status: false, project_id: params[:p5][:project_id])
     Signup.create(student_id: @student.id, priority: 6, status: false, project_id: params[:p6][:project_id])
-    @signups = @student.signups
     @digest=create_hash(@student)
     EnrollmentMail.confirmation_email(@student, @digest).deliver
     render action:'show'
