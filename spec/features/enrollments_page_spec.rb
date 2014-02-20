4de0b5997d4d3041927a5c15654b928fc21b4ae7
Sallinen Anniina C <annisall@da3-cs-dk108-04.helsinki.fi>
2014-02-20 12:55:02 +0200

    wrote a test

--- spec/features/enrollments_page_spec.rb
+++ spec/features/enrollments_page_spec.rb
@@ -95,6 +95,39 @@ describe "Enrollments page" do
   #    expect(page).to have_content 'http://ilmoprojekti.herokuapp.com/enrollments/edit/' + user.id + '/' + hash 
     end
 
+    it "redirets to confirmation page containing information about signups" do
+       FactoryGirl.create :projectbundle
+
+      generate_six_unique_projects_with_user(1)
+      @projects = Project.all
+
+      expect(Project.count).to eq(6)
+
+      visit root_path
+
+      fill_in('sfirstname', with:"Testi")
+      fill_in('slastname', with:"Testinen")
+      fill_in('studentnumber', with:"1234567")
+      fill_in('email', with:"testi@testi.fi")
+
+      select('1', from:'p1[project_id]')
+      select('2', from:'p2[project_id]')
+      select('3', from:'p3[project_id]')
+      select('4', from:'p4[project_id]')
+      select('5', from:'p5[project_id]')
+      select('6', from:'p6[project_id]')
+
+      click_button('Ilmoittaudu')
+      expect(page).to have_content('1. valinta:' )
+      expect(page).to have_content('2. valinta: ')
+      expect(page).to have_content('3. valinta: p3[name]')
+      expect(page).to have_content('4. valinta: p4[name]')
+      expect(page).to have_content('5. valinta: p5[name]')
+      expect(page).to have_content('6. valinta: p6[name]')
+    end
+
+ 
+
   end
 
 end
