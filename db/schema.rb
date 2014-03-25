307b7290106f013de3a63ae140f08a8aca6809dc
Mika Aho <mjaho@cs.helsinki.fi>
2014-03-25 14:17:42 +0200

    Verifiointikamaa projectbundlelle

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320155317) do
+ActiveRecord::Schema.define(version: 20140325112012) do
 
   create_table "enrollments", force: true do |t|
     t.string   "firstname"
