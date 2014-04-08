70a77c292e1b114ca47fb1a11bf5dfdc1fb0dbe6
Henri Karhu <karhu.henri@gmail.com>
2014-04-08 16:58:23 +0300

    bundle_page testejä

--- spec/features/projectbundle_page_spec.rb
+++ spec/features/projectbundle_page_spec.rb
@@ -24,8 +24,44 @@ describe "Projectpundle page" do
 
       expect(page).to have_unchecked_field("projectbundle_active")
       expect(page).to have_field("projectbundle_description")
+      expect(page).to have_button("Tallenna projektiryhmä")
+      expect(page).to have_link("Takaisin")
+    end
+    it "should go to own page after greating valid bundle" do
+      user = FactoryGirl.create(:admin)
+      signin(username:user.username, password:user.password)
+      visit new_projectbundle_path;
+      fill_in("projectbundle_name", with: 'testi')
+      fill_in("projectbundle_description", with: 'testingtesting')
+
+      click_button("Tallenna projektiryhmä")
+
+      expect(page).to have_content "Projektiryhmä onnistuneesti luotu."
+
 
     end
 
+    it "should not create new bundle after greating with empty name field" do
+      user = FactoryGirl.create(:admin)
+      signin(username:user.username, password:user.password)
+      visit new_projectbundle_path;
+      fill_in("projectbundle_name", with: '')
+      fill_in("projectbundle_description", with: 'testingtesting')
+
+      click_button("Tallenna projektiryhmä")
+
+      expect(page).to have_content "Name can't be blank"
+    end
+    it "should not create new bundle after greating with empty description field" do
+      user = FactoryGirl.create(:admin)
+      signin(username:user.username, password:user.password)
+      visit new_projectbundle_path;
+      fill_in("projectbundle_name", with: 'testi')
+      fill_in("projectbundle_description", with: '')
+
+      click_button("Tallenna projektiryhmä")
+
+      expect(page).to have_content "Description can't be blank"
+      end
     end
 end
