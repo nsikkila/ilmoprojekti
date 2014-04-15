e1981908430888f3ee4013b3febd440a1394c4e6
annisall <anniina.sallinen@cs.helsini.fi>
2014-04-15 12:36:39 +0300

    added tests for email

--- spec/mailers/enrollment_mail_spec.rb
+++ spec/mailers/enrollment_mail_spec.rb
@@ -4,18 +4,24 @@ include TestHelper
 describe EnrollmentMail do
   describe "confirmation email " do
 
-    before(:all) do
-  #    FactoryGirl.create(:projectbundle)
-  #    generate_six_unique_projects(1)
-  #    @email = FactoryGirl.create(:enrollment, email:"jepa@gmail.com")
+    before(:each) do
+    # @bundle=FactoryGirl.create(:projectbundle)
+    #  generate_six_unique_projects(@bundle.id)
+      @enrollment =create_enrollment_with_signups
+      @email=EnrollmentMail.confirmation_email(@enrollment, "tamaontestihash", @enrollment.projects.first.projectbundle)
+    #  @email = FactoryGirl.create(:enrollment, email:"jepa@gmail.com")
     end
 
     it "should have right recipient" do
-    #  @email.should deliver_to("jepa@gmail.com")
+      @email.should deliver_to("test@email.com")
     end
 
-    it "should have correct user id" do
+    it "should have correct link" do
+      @email.should have_body_text("http://ilmoprojekti.herokuapp.com/enrollments/"+@enrollment.id.to_s+"/tamaontestihash")
+    end
 
+    it "should have correct subject" do
+      @email.should have_subject("Vahvistus ilmottautumisesta")
     end
 
   end
