3f4ac79c6be6c82d1ff0fbe46c7f3e24ad66a33a
Henri Karhu <karhu.henri@gmail.com>
2014-05-02 22:52:29 +0300

    korjasin päivämäärät ja poistin turhan tiedoston

--- config/initializers/datetime_formats.rb
+++
@@ -1,5 +0,0 @@
-#Time::DATE_FORMATS[:laurea_way] = "%m %Y %H.%M"
-#Time::DATE_FORMATS[:laurea_way] = lambda { |time| time.strftime("%B #{time.day.ordinalize}") }
-
-Date::DATE_FORMATS[:default] = "%d/%m/%Y"
-Time::DATE_FORMATS[:default] = "%d/%m/%Y %H:%M"
